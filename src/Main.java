d795ce840e0161b8fcae3fb2a8df2496552837a6
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-12 19:40:28 +0300

    Add corrections in 4 task

--- src/Main.java
+++ src/Main.java
@@ -27,15 +27,15 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
         //4 задача
-        int deliveryDistance = 15;
+        int deliveryDistance = 50;
         if (deliveryDistance > 20 && deliveryDistance <= 60){
-            System.out.println("Потребуется дней: " + 3);
+            System.out.println("Потребуется дней: " + 2);
         }
         if (deliveryDistance >= 1 && deliveryDistance < 20){
             System.out.println("Потребуется дней: " + 1);
         }
         if (deliveryDistance > 60 && deliveryDistance <= 100){
-            System.out.println("Потребуется дней: " +4 );
+            System.out.println("Потребуется дней: " +3);
         }
         if (deliveryDistance>100){
             System.out.println("Доставки нет");
